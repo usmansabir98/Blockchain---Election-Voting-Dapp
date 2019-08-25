5ca4364b9f177e370837916e6dd41b5c11d99f6c
usmansabir98 <usmansabir98@hotmail.com>
2019-08-26 03:03:36 +0500

    Cast votes complete

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,8 +9,14 @@ contract Election {
     	uint voteCount;
     }
 
+    mapping(address => bool) public voters;
+
     mapping(uint => Candidate) public candidates;
     uint public candidatesCount;
+
+    event votedEvent(
+    	uint indexed _candidateId
+    );
 
     // Constructor
     constructor () public {
@@ -21,5 +27,17 @@ contract Election {
     function addCandidate(string memory _name) private {
     	candidatesCount++;
     	candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+    	require(!voters[msg.sender]);
+
+    	require(_candidateId>0 && _candidateId<=candidatesCount);
+
+    	voters[msg.sender] = true;
+
+    	candidates[_candidateId].voteCount ++;
+
+    	emit votedEvent(_candidateId);
     }
 }
