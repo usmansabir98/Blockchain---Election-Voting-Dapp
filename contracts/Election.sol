eb40597ccf42f289339df000a510da600c539afe
usmansabir98 <usmansabir98@hotmail.com>
2019-08-25 22:23:43 +0500

    add Election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,10 +3,23 @@ pragma solidity ^0.5.0;
 contract Election {
     // Store candidate
     // Read candidate
-    string public candidate;
+    struct Candidate{
+    	uint id;
+    	string name;
+    	uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
 
     // Constructor
     constructor () public {
-        candidate = "Candidate 1";
+    	addCandidate('Candidate 1');
+    	addCandidate('Candidate 2');
+    }
+
+    function addCandidate(string memory _name) private {
+    	candidatesCount++;
+    	candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
